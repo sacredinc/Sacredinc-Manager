88fe9f6a3b778e8ce3d1cfcf28449b9d791d2450
hfgd123 <46094961+hfgd123@users.noreply.github.com>
2022-02-06 23:32:01 +0100

    added buttons to server command

--- commands/server.js
+++ commands/server.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 
 module.exports = {
@@ -6,8 +7,27 @@ module.exports = {
 		.setName('server')
 		.setDescription('Verwaltet einen Server!'),
 	async execute(interaction) {
-		await interaction.deferReply({ ephemeral: true });
-		await wait(1);
-		await interaction.editReply('Pong!');
+		const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('add')
+                .setLabel('Hinzufügen')
+                .setStyle('SUCCESS')
+                .setEmoji('➕')
+                .setDisabled(true))
+            .addComponents(
+                new MessageButton()
+                .setCustomId('edit')
+                .setLabel('Bearbeiten')
+                .setStyle('SECONDARY')
+                .setDisabled(true)
+            .addComponents(
+                new MessageButton()
+                .setCustomId('remove')
+                .setLabel('Entfernen')
+                .setStyle('DANGER')
+                .setEmoji('❌')
+                .setDisabled(true))
+            )
 	},
 };
