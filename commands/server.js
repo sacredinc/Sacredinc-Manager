66db595dde34eee1aec42d9306151a69f566a4d7
hfgd123 <46094961+hfgd123@users.noreply.github.com>
2022-02-07 11:03:49 +0100

    Switched from buttons to subcommands.

--- commands/server.js
+++ commands/server.js
@@ -5,31 +5,57 @@ const wait = require('util').promisify(setTimeout);
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Verwaltet einen Server!'),
-	async execute(interaction) {
-		const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                .setCustomId('add')
-                .setLabel('Hinzufügen')
-                .setStyle('SUCCESS')
-                .setEmoji('➕')
-                .setDisabled(true))
-            .addComponents(
-                new MessageButton()
-                .setCustomId('edit')
-                .setLabel('Bearbeiten')
-                .setStyle('SECONDARY')
-                .setDisabled(true))
-            .addComponents(
-                new MessageButton()
-                .setCustomId('remove')
-                .setLabel('Entfernen')
-                .setStyle('DANGER')
-                .setEmoji('❌')
-                .setDisabled(true))
+		.setDescription('Verwaltet einen Server!')
+        .addSubcommand(subcommand =>
+            subcommand 
+            .setName('add')
+            .setDescription('Füge den aktuellen Server in die Datenbank hinzu')
+            .addStringOption(option =>
+                option.setName('Servername')
+                    .setDescription('Der vollständige Name des Servers')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('Serverkürzel')
+                    .setDescription('Der abgekürzte Name des Servers')
+                    .setRequired(true))
+            .addIntegerOption(option =>
+                option.setName('Serverrang')
+                    .setDescription('Der Netzwerkrang des Servers')
+                    .setRequired(true)
+                    .addChoice('Zentralserver', 0)
+                    .addChoice('Verifizierter Server', 1)
+                    .addChoice('Netzwerkmitglied', 2)
+                    .addChoice('Kooperationspartner', 3)
+                    .addChoice('Partner', 4)
+                    .addChoice('Normaler Server', 5)
+                    .addChoice('Gesperrter Server', 99)))
+        .addSubcommand(subcommand =>
+            subcommand 
+            .setName('remove')
+            .setDescription('Entferne einen Server aus der Datenbank')
+            .addIntegerOption(option =>
+                option.setName('Serverkürzel')
+                .setDescription('Das Serverkürzel des zu entfernenden Servers')
+                .setRequired(true))),
 
-            await interaction.reply({ content: 'Servermanagment gestartet!', components: [row] });
+	async execute(interaction) {
+        if(interaction.options.getSubcommand() === 'add') {
+            const servername = interaction.options.getString('Servername');
+            const serverkuerzel = interaction.options.getString('Serverkürzel');
+            const serverrang = interaction.options.getInteger('Serverrang');
+            const serverid = interaction.guild.id;
+
+
+            await interaction.reply(`${servername} (${serverid}) wurde mit dem Kürzel ${serverkuerzel} auf Rang ${serverrang} hinzugefügt.`)
+
+        }
+
+        else if(interaction.options.getSubcommand() === 'remove') {
+            const serverkürzel = interaction.options.getString('Serverkürzel');
+
+        }
 
 	},
+    
+    
 };
