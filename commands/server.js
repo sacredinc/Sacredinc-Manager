335dba46a0e628b8ec3fc3ba8503d331d22786d3
hfgd123 <46094961+hfgd123@users.noreply.github.com>
2022-02-09 13:20:38 +0100

    /server remove now works. IDs no longer increase when a server is edited

--- commands/server.js
+++ commands/server.js
@@ -15,7 +15,7 @@ module.exports = {
 		.setDescription('Verwaltet einen Server!')
         .addSubcommand(subcommand =>
             subcommand 
-            .setName('add')
+            .setName('set')
             .setDescription('Füge den aktuellen Server in die Datenbank hinzu')
             .addStringOption(option =>
                 option.setName('servername')
@@ -40,13 +40,13 @@ module.exports = {
             subcommand 
             .setName('remove')
             .setDescription('Entferne einen Server aus der Datenbank')
-            .addIntegerOption(option =>
+            .addStringOption(option =>
                 option.setName('serverkürzel')
                 .setDescription('Das Serverkürzel des zu entfernenden Servers')
                 .setRequired(true))),
 
 	async execute(interaction) {
-        if(interaction.options.getSubcommand() === 'add') {
+        if(interaction.options.getSubcommand() === 'set') {
             servername = interaction.options.getString('servername');
             serverkuerzel = interaction.options.getString('serverkürzel');
             serverrang = interaction.options.getInteger('serverrang');
@@ -65,6 +65,8 @@ module.exports = {
                     port: db_port,
                 });
 
+                await clearServer(conn);
+
                 const rows = await getId(conn);
 
                 for (let i = 0, len = rows.length; i < len; i++) {
@@ -82,6 +84,30 @@ module.exports = {
         }
 
         else if(interaction.options.getSubcommand() === 'remove') {
+            serverkuerzel = interaction.options.getString('serverkürzel');
+
+            await interaction.deferReply({ ephemeral: true })
+
+            let conn;
+
+            try {
+                conn = await mariadb.createConnection({
+                    host: db_ip,
+                    user: db_user,
+                    password: db_pw,
+                    database: db_db,
+                    port: db_port,
+                });
+
+                await removeServer(conn);
+
+            } catch (err) {
+                console.log(err);
+            } finally {
+                if (conn) conn.close();
+            }
+
+            await interaction.editReply(`Der Server ${serverkuerzel} wurde entfernt.`)
 
         }
 
@@ -93,6 +119,17 @@ module.exports = {
             return conn.query(`INSERT INTO servers (id, name_long, name_short, server_rank, discord_id) VALUES ('${idNew}', '${servername}', '${serverkuerzel}', '${serverrang}', '${serverid}');`);
         }
 
+        function clearServer(conn) {
+            let check = conn.query(`SELECT discord_id FROM network.servers WHERE name_short='${serverkuerzel}'`);
+            if (check.length !== 0) {
+                conn.query(`DELETE FROM servers WHERE name_short='${serverkuerzel}'`);
+                console.log(`Deleted ${serverkuerzel} from the database to make space for new entry`)
+            }
+        }
+
+        function removeServer(conn) {
+            return conn.query(`UPDATE servers SET server_rank = 1 WHERE name_short = '${serverkuerzel}'`);
+        }
 
 }
     
